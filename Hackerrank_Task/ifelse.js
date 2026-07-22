19b162c5889f7d221b45dedf10a3719b07f6edc9
rupali kolekar <rupsy77@users.noreply.github.com>
2026-07-23 00:05:39 +0530

    Add Hackerrank_Task exercises and project config
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Hackerrank_Task/ifelse.js
@@ -0,0 +1,35 @@
+//let input = 85; // change this value
+console.log("Enter the mark:");
+const input = require('fs').readFileSync(0,'utf8');
+if(input>90 && input<=100)
+{
+    console.log("AA");
+}
+else if(input>80 && input<= 90)
+{
+    console.log("AB");
+}
+else if(input>70 && input <= 80)
+{
+    console.log("BB");
+}
+else if(input>60 && input <= 70)
+{
+    console.log("BC");
+}
+else if(input>50 && input <= 60)
+{
+    console.log("CC");
+}
+else if(input>40 && input <= 50)
+{
+    console.log("CD");
+}
+else if(input>30 && input <= 40)
+{
+    console.log("DD");
+}
+else 
+{
+    console.log("FF");
+}
